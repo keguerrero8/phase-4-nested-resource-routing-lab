5d3c2c9440577ec452705f2b99da4f201ededc90
Kevin Guerrero <guerrk83z@gmail.com>
2022-02-02 07:04:03 -0500

    commit tree c5949ec307369fbd29a0b46807b2ba19143781c9

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,37 @@
 class ItemsController < ApplicationController
 
   def index
-    items = Item.all
-    render json: items, include: :user
+    if params[:user_id]
+      user = User.find(params[:user_id])
+      items = user.items
+      render json: items
+    else 
+      items = Item.all
+      render json: items, include: :user
+    end
+  rescue ActiveRecord::RecordNotFound => e
+    render status: :not_found
+  end
+
+  def show
+    item = Item.find(params[:id])
+    render json: item, status: :ok
+  rescue ActiveRecord::RecordNotFound => e
+    render status: :not_found
+  end
+
+  def create
+    user = User.find(params[:user_id])
+    # item = Item.create(params_valid)
+    # user.items << item
+    item = user.items.create(params_valid)
+    render json: item, status: 201
+  end
+
+  private
+
+  def params_valid
+    params.permit(:name, :description, :price)
   end
 
 end
